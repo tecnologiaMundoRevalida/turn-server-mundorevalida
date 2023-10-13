964b23ca5cf26bb02da366f9e5ef21d15586caa6
Elder Oliveira Ext <elder.oliveira.ext@infracommerce.com.br>
2023-10-13 08:01:41 -0300

    Add turn server port 80

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const Turn = require('node-turn');
 const server = new Turn({
   // Listening IP and port for the TURN server
   listeningIps: ['0.0.0.0'], // Listen on all available interfaces
-  listeningPort: 3478,
+  listeningPort: 80,
 
   // Your server's public IP (for NAT)
   // Replace this with your actual server's public IP
   // If your server has a public IP, you can use it directly
   // If your server is behind a NAT, use the public IP of the NAT
   // You can also use a domain name here
-  publicAddress: '44.196.233.187',
+  publicAddress: '54.242.16.164',
 
   // TURN server credentials
   // You should generate your own username and password
@@ -22,5 +22,5 @@ const server = new Turn({
 });
 
 server.start(() => {
-  console.log('TURN server is running on port 3478');
+  console.log('TURN server is running on port 80');
 });
